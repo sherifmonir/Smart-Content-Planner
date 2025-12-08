1630168ca1c0b971df5fbc85fe76f4491a88b435
Sherif Monir <sherif.monir.badreddin@gmail.com>
2025-12-08 09:25:00 +0200

    components changes

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header(){
+    return (
+        <header className="header">
+            {/*logo*/}
+            {/*search bar*/}
+            {/* new button*/}
+        </header>
+    )
+}
