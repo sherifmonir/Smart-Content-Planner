23b86f28855460ac9f16f881a9b09189aafed358
Sherif Monir <sherif.monir.badreddin@gmail.com>
2025-12-10 08:09:15 +0200

    Update progress for today

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import CalendarPage from "./pages/CalendarPage";
 export default function App(){
     return (
         <>
-        <h1>Smart Content Planer</h1>
+        
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Dashboard />} />
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 
 export default function SideBar(){
     return (
-        <aside className="sidebar">
+    <aside className="sidebar">
+        <div className= "logo-name">
             <h1>SideBar</h1>
-            <a href="/"></a>
-            <a href="/editor"></a>
-            <a href="/calendar"></a>
-        </aside>
+        </div>
+        <div className="navigation-bar">
+            <a href="/">Dashboard</a>
+            <a href="/editor">Calendar</a>
+            <a href="/calendar">Editor</a>
+        </div>
+    </aside>
     )
 }
