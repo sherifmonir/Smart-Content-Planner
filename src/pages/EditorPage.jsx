796b4b1614a6c3a49d03f7b2370d9799eccd5660
Sherif Monir <sherif.monir.badreddin@gmail.com>
2025-12-20 17:21:00 +0200

    Editor Page updates

--- src/pages/EditorPage.jsx
+++ src/pages/EditorPage.jsx
@@ -13,14 +13,27 @@ export default function EditorPage(){
                     
             <SideBar />
             <TopBar />
-            <main className="Main Editor-Page">
-               
+            <main className="Main ">
+               <section className="Editor-Page">
 
-                <section classsName="Write-Post"></section>
-                <section classsName="Add-Image"></section>
-                <section classsName="Platform-And-date"></section>
-                <section classsName="Write-Post-Buttons"></section>
+                <section className="Write-Post">
+                    <p>Font Edits goes here</p>
+                    <textarea></textarea>
+                </section>
 
+                <section className="Add-Image">
+                    <span>+Add Image</span>
+                    </section>
+
+                <section className="Platform-And-Date">Platform and Date goes here</section>
+                <section className="Write-Post-Buttons">
+
+                    <button className="Save-as-Draft-Btn">Save as Draft</button>
+                    <button className="Schedule-Post-Btn">Schedule Post</button>
+
+                </section>
+                     
+             </section>
             </main>
         </div>
         
